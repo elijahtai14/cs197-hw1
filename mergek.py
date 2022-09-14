c517ee752bfd318393f51a3a0faaf9a9fc38accc
elijahtai14 <43102561+elijahtai14@users.noreply.github.com>
2022-09-14 14:11:16 -0400

    Applying this commit will make the previous priority queue work with empty inputs.
    The idea is that sometimes the user might make a
    mistake and one of the lists could be empty,
    and we want to handle this gracefully. Used debugging
    tools to find my bugs!

--- mergek.py
+++ mergek.py
@@ -9,21 +9,27 @@ class ListNode:
 
 class Solution:
     def list_to_list_node(self, list):
+        
         if len(list) == 0:
             return None
-        if len(list) == 1:
-            return ListNode(val = list[0])
-        node = ListNode(val = list[0])
-        node.next = self.list_to_list_node(list[1:])
-        return node
+        
+        head = ListNode(val = list[0])
+        cur = head
 
+        for i in range(1, len(list)):
+            cur.next = ListNode(val = list[i])
+            cur = cur.next
+        
+        return head
+    
     def mergeKLists(self, lists):
         res = []
         def fill_pq(PQ, heads):
             for i in range(len(heads)):
-                PQ.put((heads[i].val, i))
-                heads[i] = heads[i].next
-
+                if not heads[i] is None:
+                    PQ.put((heads[i].val, i))
+                    heads[i] = heads[i].next
+        
         PQ = PriorityQueue(maxsize=len(lists))
         heads = [node for node in lists]
         fill_pq(PQ, heads)
@@ -34,24 +40,23 @@ class Solution:
             if heads[index]:
                 PQ.put((heads[index].val, index))
                 heads[index] = heads[index].next
-        
-        return self.list_to_list_node(res)
+        a = self.list_to_list_node(res)
+        return a
     
     def print_linked_list(self, node):
         temp = node
-        while temp:
-            print(temp)
+        while not temp is None:
+            print(temp.val)
             temp = temp.next
+            
     
 def main():
     s = Solution()
     
-    example_input = [s.list_to_list_node([1, 5, 9, 14]), 
-                     s.list_to_list_node([2, 3, 4, 8]), 
-                     s.list_to_list_node([5, 6, 7, 9, 15])]
+    example_input = [s.list_to_list_node([0, 2, 5])]
 
-    res = s.mergeKLists(example_input)
+    r = s.mergeKLists(example_input)
     
-    s.print_linked_list(res)
+    s.print_linked_list(r)
 
 main()
